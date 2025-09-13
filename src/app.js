6303b568e403ca7fe59ce1a45fe6d2edc1e0ffdc
hrshdp <harshdeepchandrakar9@gmail.com>
2025-09-13 10:26:16 +0530

    - Added database connection logic and intialize express sever with health check endpoint.

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import pool from "./db/connection.js";
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT 1 + 1 AS result");
+        res.json({ status: "ok", db: rows[0].result });
+    } catch (err) {
+        res.status(500).json({status: "error", error: err.message });
+    }
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
